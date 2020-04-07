c4b4e78ccc3187607c48cdebe06fb4619d496766
Shane-Bowen <shane.bowen@mycit.ie>
2020-04-07 14:23:42 +0100

    Modified File

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 # Import libraries
+import os
 import numpy as np
 from numpy import concatenate
 from math import sqrt
@@ -7,32 +8,32 @@ from pandas import read_csv
 from pandas import DataFrame
 from pandas import concat
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from datetime import datetime, timedelta
 from tensorflow import keras
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning) 
 
-def reframe_data(data, n_days):
-    n_features = data.shape[1]
-    cols = []
-    arr = []
-    np_cols = []
+# def reframe_data(data, n_days):
+#     n_features = data.shape[1]
+#     cols = []
+#     arr = []
+#     np_cols = []
         
-    for i in range(0, df.shape[0]):
-        for j in range(i, i+((n_days * 7) + 1), n_days):
-            if j < data.shape[0]:
-                arr.append(data[j])
-            else:
-                return DataFrame(np_cols)
+#     for i in range(0, df.shape[0]):
+#         for j in range(i, i+((n_days * 7) + 1), n_days):
+#             if j < data.shape[0]:
+#                 arr.append(data[j])
+#             else:
+#                 return DataFrame(np_cols)
         
-        cols.append(arr)
-        np_cols = np.array(cols)
-        np_cols = np_cols.reshape(np_cols.shape[0], (n_days+1)*n_features)
-        arr = []
-    
+#         cols.append(arr)
+#         np_cols = np.array(cols)
+#         np_cols = np_cols.reshape(np_cols.shape[0], (n_days+1)*n_features)
+#         arr = []
 
 # convert series to supervised learning & normalize input variables
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
@@ -63,64 +64,97 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     return agg
 
 #get previous inputs from date
-def get_previous_inputs(date, company_id, df, n_days, n_features):
-    
-    delta = timedelta(days=1)
+def get_input_features(df, date, company_id, input_data, prediction_data):
+
+    # specify the number of days and features 
+    n_days = 7
+    n_features = df.shape[1]
+
+    # numpy array for previous inputs
+    input_features = np.array([])
+
+    # target_date & cur_date
     target_date = datetime.strptime(date, "%Y-%m-%d")
-    start_date = target_date - timedelta(days=n_days)
-    previous_inputs = np.array([])
+    cur_date = target_date - timedelta(days=n_days)
+        
+    # get end_date stored in dataframe
+    end_date = datetime.strptime(df.tail(1).index.item(), "%Y-%m-%d")
     
-    #df = df.loc[df['company_id'] == company_id]
-    
-    # get last_date in stored in dataframe
-    last_date = datetime.strptime(df.tail(1).index.item(), "%Y-%m-%d")
-    
-    # if target_date is less than last_date then we can get previous inputs
-    if target_date <= last_date + delta:    
-        while start_date < target_date:
-            cur_date = start_date.strftime("%Y-%m-%d")
-            previous_inputs = np.append(previous_inputs, [df.loc[cur_date]])
-            start_date += delta
-    # else we will have to push back target and start date until inside dataframe range
-    else:
-        while target_date > last_date + delta:
-            target_date -= timedelta(days=7)
-            start_date -= timedelta(days=7)
-        while start_date < target_date:
-            cur_date = start_date.strftime("%Y-%m-%d")
-            previous_inputs = np.append(previous_inputs, [df.loc[cur_date]])
-            start_date += delta
+    # while loop until target_date
+    while cur_date < target_date:
+        if cur_date <= end_date:
+            input_features = np.append(input_features, [df.loc[cur_date.strftime("%Y-%m-%d")]])
+            input_data[cur_date.strftime("%Y-%m-%d")] = df.loc[cur_date.strftime("%Y-%m-%d")]['volume_tests']
+            cur_date += timedelta(days=1)       
+        else:
+            weekday = [0, 1, 2, 3, 4]
+            # check if cur_date is weekend
+            if cur_date.weekday() in weekday:
+                isWeekend = 0
+            else:
+                isWeekend = 1
+                
+            # get last_date in df on that day
+            last_date = cur_date
+            while last_date >= end_date:
+                last_date -= timedelta(days=7)
+            last_date = last_date.strftime('%Y-%m-%d')
             
-    previous_inputs = previous_inputs.reshape((n_days, n_features))
+            if 'min_commit' in df.columns:
+                input_features = np.append(input_features, [prediction_data[cur_date.strftime('%Y-%m-%d')], isWeekend, df.loc[last_date]['quality_too_poor'], df.loc[last_date]['number_busy'],
+                                                              df.loc[last_date]['temporarily_unable_test'], df.loc[last_date]['outage_hrs'], df.loc[last_date]['number_test_types'], df.loc[last_date]['numbers_tested'], df.loc[last_date]['min_commit']])
+            else:
+                input_features = np.append(input_features, [prediction_data[cur_date.strftime('%Y-%m-%d')], isWeekend, df.loc[last_date]['quality_too_poor'], df.loc[last_date]['number_busy'],
+                                                              df.loc[last_date]['temporarily_unable_test'], df.loc[last_date]['outage_hrs'], df.loc[last_date]['number_test_types'], df.loc[last_date]['numbers_tested']])
+            cur_date += timedelta(days=1)
+        
+    # reshape input_features
+    input_features = input_features.reshape((n_days, n_features))
+    
+    # normalize features
+    scaler = MinMaxScaler(feature_range=(0, 1))
     
     #normalize previous_input features
-    previous_inputs = scaler.fit_transform(previous_inputs)
-    previous_inputs = previous_inputs.reshape((1, n_days, n_features))
+    input_features = scaler.fit_transform(input_features)
+    input_features = input_features.reshape((1, n_days, n_features))
     
-    return previous_inputs
+    return input_features, scaler, input_data
+
+#invert scale prediction
+def invert_scailing(input_features, prediction, scaler):
+    
+    # specify the number of days and features 
+    n_days = 7
+    n_features = input_features.shape[2]
+    
+    # reshape input_features
+    input_features = input_features.reshape((1, n_days*n_features))
+    
+    # invert scaling for forecast
+    inv_prediction = concatenate((prediction, input_features[:, -(n_features-1):][0:1]), axis=1)
+    inv_prediction = scaler.inverse_transform(inv_prediction)
+    inv_prediction = inv_prediction[:,0]
+    
+    return inv_prediction
 
 company_list = [1, 2, 6, 9, 17]
 #company_list = [2]
+#company_list = []
+#company_list = [17]
 
 for company in company_list:
     
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, """reports/company_report_{0}.csv""".format(company))
     # load dataset
-    df = read_csv(f'./reports/company_report_{company}.csv', header=0, index_col="time")
-    df = df[['volume_tests', 'company_id', 'company_type', 'is_weekend', 'quality_too_poor', 'number_busy', 'temporarily_unable_test', 'number_test_types', 'numbers_tested']]
+    df = read_csv(filename, header=0, index_col="time")
+    df = df[['volume_tests', 'date', 'month', 'is_weekend', 'quality_too_poor', 'number_busy', 'temporarily_unable_test', 'outage_hrs', 'number_test_types', 'numbers_tested', 'min_commit']]
+    df = df.dropna(axis='columns')
     values = df.values
         
     # specify the number of days and features 
     n_days = 7
     n_features = df.shape[1]
-    
-    # integer encode direction
-    encoder = LabelEncoder()
-    # enocde company_type
-    values[:,2] = encoder.fit_transform(values[:,2])
-    df.iloc[:,[2]] = encoder.fit_transform(df.iloc[:,[2]])
-    
-    #values[:,6] = encoder.fit_transform(values[:,6])
-    #df.iloc[:,[6]] = encoder.fit_transform(df.iloc[:,[6]])
     
     #slice first n_days (no prediction made)
     df = df[n_days:]
@@ -134,7 +168,6 @@ for company in company_list:
     
     # frame as supervised learning
     reframed = series_to_supervised(scaled, n_days, 1)
-    
     #reframed = reframe_data(scaled, n_days)
     
     # split into train and test sets
@@ -185,51 +218,65 @@ for company in company_list:
     print('Test RMSE: %.3f' % rmse)
     
     # Save the model
-    model.save(f'./models/model_{company}.h5')
+    filename = os.path.join(dirname, """models/model_{0}.h5""".format(company))
+    model.save(filename)
+    
+    print(company)
+
+    # plot actual vs prediction
+    num_days = 210
+    pyplot.plot(list(inv_y[:num_days]), label='actual')
+    pyplot.plot(inv_yhat[:num_days], label='prediction')
+    pyplot.legend()
+    pyplot.show()
 
 # =============================================================================
-#     # plot actual vs prediction
-#     num_days = 210
-#     pyplot.plot(list(inv_y[:num_days]), label='actual')
-#     pyplot.plot(inv_yhat[:num_days], label='prediction')
-#     pyplot.legend()
-#     pyplot.show()
-# =============================================================================
-
-# =============================================================================
-# # Make a prediction test
+# # Make a prediction
 # 
 # # inputs
-# date = '2020-01-15'
-# company = 2
+# date = '2020-04-07'
+# company_id = '2'
 # 
-#  # load dataset
-# df = read_csv(f'./reports/company_report_{company}.csv', header=0, index_col="time")
-# df = df[['volume_tests', 'company_id', 'company_type', 'is_weekend', 'quality_too_poor', 'number_busy', 'temporarily_unable_test', 'number_test_types', 'numbers_tested']]
+# # store all input data and prediction data
+# input_data = {}
+# prediction_data = {}
 # 
-# df.iloc[:,[2]] = encoder.fit_transform(df.iloc[:,[2]])
+# # get model for that company
+# model = keras.models.load_model(f"./models/model_{company_id}.h5")
 # 
-# #slice first n_days (no prediction made)
-# df = df[n_days:]
+# # load dataset
+# df = read_csv(f'./reports/company_report_' + company_id + '.csv', header=0, index_col="time")
+# df = df[['volume_tests', 'is_weekend', 'quality_too_poor', 'number_busy', 'temporarily_unable_test', 'outage_hrs', 'number_test_types', 'numbers_tested', 'min_commit']]
+# df = df.dropna(axis='columns')
 # 
-# # get stored model
-# model = keras.models.load_model(f"./models/model_{company}.h5")
+# # get last date in dataframe
+# cur_date = datetime.strptime(df.tail(1).index.item(), "%Y-%m-%d")
 # 
-# # get previous_inputs
-# previous_inputs = get_previous_inputs(date, company, df, n_days, n_features)
+# if cur_date < datetime.strptime(date, '%Y-%m-%d'):
 # 
-# # make a prediction
-# yhat = model.predict(previous_inputs[0:1])
-# test_X = test_X.reshape((test_X.shape[0], n_days*n_features))
-#  
-# # invert scaling for forecast
-# inv_yhat = concatenate((yhat, test_X[:, -(n_features-1):][0:1]), axis=1)
-# inv_yhat = scaler.inverse_transform(inv_yhat)
-# inv_yhat = inv_yhat[:,0]
-# print("Prediction", int(round(inv_yhat[0])))
-# #print("Actual", df.loc[date]['volume_tests'])
+#     # while loop until date selected
+#     while cur_date < datetime.strptime(date, '%Y-%m-%d'):
+#     
+#         # iterate cur_date
+#         cur_date += timedelta(days=1)
+#     
+#         input_features, scaler, input_data = get_input_features(df, cur_date.strftime('%Y-%m-%d'), company_id, input_data, prediction_data)
+#         prediction = model.predict(input_features)
+#                 
+#         inv_prediction = invert_scailing(input_features, prediction, scaler)
+#         prediction_data[cur_date.strftime('%Y-%m-%d')] = int(round(inv_prediction[0]))
+#         
+#         print(input_data)
+#         print(prediction_data)
+# 
+# else:
+#     # iterate cur_date
+#     cur_date += timedelta(days=1)
+#     
+#     input_features, scaler, input_data = get_input_features(df, cur_date.strftime('%Y-%m-%d'), company_id, input_data, prediction_data)
+#     print(input_data)
 # =============================================================================
-
+    
 # =============================================================================
 # if not os.path.isdir('models'):
 #     os.mkdir('models') 
